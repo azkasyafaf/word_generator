276cfb6234fa0448ab02051163ebff9fd9583a38
Azka Syafa Firdaus <azkasyafaf@gmail.com>
2021-02-18 03:33:36 +0700

    Create a new version of main.py
    Created new start function to encapsulate the try ticket. Moving it from generate function in Application class. Also changing the result print statement, so result will be sorted ascending.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from itertools import permutations
 import enchant
 import os
 
+running = True
+
 class Application():
     def __init__(self):
         self.inp = []
@@ -9,39 +11,47 @@ class Application():
         self.op = set()
     
     def generator(self, inp, minLength = 2):
-        temp_out = ""
+        self.op.clear()
+
         for n in range(len(inp)):
             for word in list(permutations(inp,n+1)):
                 temp_out = "".join(word)
                 if len(temp_out) > minLength:
                     if self.d.check(temp_out):
                         self.op.add(temp_out)
-        print(sorted(self.op, key=len))
+        
+        print(sorted(self.op, key=len, reverse=False))
         print("\nWords found: {}".format(len(self.op)))
 
+def start(app):
+    temp_inp = input("Input your words: ")
+    min_length = input("What is the minimum lenght? ")
+
+    app.inp = [x.lower() for x in temp_inp]
+    min_length = 2 if (min_length == "") else int(min_length)
+
+    print("\n--------------\n")
+    app.generator(app.inp, min_length)
+    print("\n--------------\n")
+
 def main():
-    while True:
-        app = Application()
-        temp_inp = input("Input your words: ")
-        min_length = input("What is the minimum lenght? ")
+    global running
 
-        app.inp = [x.lower() for x in temp_inp]
-        min_length = 2 if (min_length == "") else int(min_length)
+    while running:
+        start(Application())
 
-        print("\n--------------\n")
-        app.generator(app.inp, min_length)
-        
         while True:
-            temp_inp = input("\n--------------\n\nDo you want to try again? (Y/N) ")
-            if temp_inp.upper() == "N" or temp_inp.upper() == "Y":
-                print("\nPlease wait...\n")
+            temp_inp = input("\nDo you want to try again? (Y/N) ")
+            print("\nPlease wait...\n")
+            
+            if temp_inp.upper() == "N":
+                running = False
                 break
-        
-        if temp_inp.upper() == "N":
-            print("Exiting...\n\n")
-            break
-        
-        os.system("clear")
-        app.op.clear()
-
+            elif temp_inp.upper() == "Y":
+                os.system("clear")
+                break
+            else:
+                print("Error. Command not found")
+    print("Exiting...\n\n")
+    
 main()
